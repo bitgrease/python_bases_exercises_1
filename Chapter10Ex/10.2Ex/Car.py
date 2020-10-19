af42498e6981b2cb276576cd1b363820241de9f8
Kelly Netterville <bitgrease@gmail.com>
2020-10-18 20:19:44 -0400

    Update Car.py
    Added drive instance method to Car class. Final exercise in Chp 10.2.

--- Chapter10Ex/10.2Ex/Car.py
+++ Chapter10Ex/10.2Ex/Car.py
@@ -5,10 +5,18 @@ class Car:
         self.mileage = mileage
 
     def __str__(self):
-        return f"The {self.color} car has {self.mileage:,d}"
+        return f"The {self.color} car has {self.mileage:,d} miles"
+
+    def drive(self, miles_driven):
+        self.mileage += miles_driven
 
 blue_car = Car("blue", 20000)
 red_car = Car("red", 30000)
+new_car = Car("brown", 0)
 
 print(blue_car)
 print(red_car)
+print(new_car)
+print("Driving new car 100 miles.")
+new_car.drive(100)
+print(new_car)
